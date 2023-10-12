1c72f766ace0dd9428cb9ca7dadb66547842e2d3
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-10-12 16:07:14 -0500

    inconsistent spacing between bg and fg components

--- src/background.js
+++ src/background.js
@@ -5,14 +5,14 @@ const Background = () => {
     return (
         <div className='background-layout'>
             <div className='background-content'>
-                <div className='background background-header' >Header</div>
-                <div className='background background-main' >Main</div>
-                <div className='background background-footer' >Footer</div>
+                <div className='background background-header'>Header</div>
+                <div className='background background-main'>Main</div>
+                <div className='background background-footer'>Footer</div>
             </div>
             <div className='background-sidebar'>
-                <div className='background background-sidebar-item' >Sidebar Item</div>
-                <div className='background background-sidebar-item' >Sidebar Item</div>
-                <div className='background background-sidebar-item' >Sidebar Item</div>
+                <div className='background background-sidebar-item'>Sidebar Item</div>
+                <div className='background background-sidebar-item'>Sidebar Item</div>
+                <div className='background background-sidebar-item'>Sidebar Item</div>
             </div>
         </div>
     )
